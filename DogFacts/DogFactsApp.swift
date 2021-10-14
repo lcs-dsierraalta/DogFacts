a43eea345904d3b71371b2ab6ccb034fd4f6972e
Diego Sierraalta <dsierraalta@lcs.on.ca>
2021-10-13 22:06:59 -0400

    Initial Commit

---
+++ DogFacts/DogFactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogFactsApp.swift
+//  DogFacts
+//
+//  Created by Diego Sierraalta on 2021-10-13.
+//
+
+import SwiftUI
+
+@main
+struct DogFactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
