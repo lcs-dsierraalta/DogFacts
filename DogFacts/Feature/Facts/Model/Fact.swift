69e465a7fe92847a112f4fb8b04e95bd1456bf44
Diego Sierraalta <dsierraalta@lcs.on.ca>
2021-10-13 23:07:08 -0400

    Added an array of dog facts as dummy data, which will allow me to preview how the facts will look

--- DogFacts/Feature/Facts/Model/Fact.swift
+++ DogFacts/Feature/Facts/Model/Fact.swift
@@ -10,3 +10,14 @@ import Foundation
 struct Fact: Decodable {
     let fact: String
 }
+
+extension Fact {
+    
+    static let dummyData: [Fact] = [
+        Fact(fact: "Dogs are awesome"),
+        Fact(fact: "Dogs are cute"),
+        Fact(fact: "Dogs are smart"),
+        Fact(fact: "People who don't like dogs are inferior to people that like dogs"),
+        Fact(fact: "Dogs are better than cats")
+    ]
+}
