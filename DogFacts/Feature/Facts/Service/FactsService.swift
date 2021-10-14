1aed4eb3014d6118336689afd1d7938f98d75d0b
Diego Sierraalta <dsierraalta@lcs.on.ca>
2021-10-14 10:03:07 -0400

    Added a service file which will interact with the API and obtain the data

--- DogFacts/Feature/Facts/Service/FactsService.swift
+++ DogFacts/Feature/Facts/Service/FactsService.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+
+protocol FactsService {
+    func fetch() async throws -> [Fact]
+}
+
+final class FactsServiceImpl: FactsService {
+    func fetch() async throws -> [Fact] {
+        let urlSession = URLSession.shared
+        let url = URL(string: APIConstants.baseUrl.appending("/api/facts"))
+        let (data, _) = try await urlSession.data(from: url!)
+        return try JSONDecoder().decode([Fact].self, from: data)
+    }
+}
